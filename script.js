e963812c718b0659bb93e7815944ab2262c9fbdd
jesslau <jesslau.14@gmail.com>
2021-02-15 14:48:56 -0800

    added .vscode script. Working on adding click event to nav menu

---
+++ script.js
@@ -0,0 +1,10 @@
+//click menu bars
+//click event toggles ul (nav) appearance or disappearance
+
+
+
+const navBar = document.getElementsByClassName("fa-bars");
+console.log(navBar);
+const ulElement = document.querySelector("ul");
+console.log(ulElement);
+
