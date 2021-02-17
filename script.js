490028ba5b339f3344eb9f61cbcc5e86d7af9807
jesslau <jesslau.14@gmail.com>
2021-02-16 21:12:49 -0800

    removed console logs

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 //Menu button
 const navBar = document.querySelector("i");
 
-navBar.addEventListener("click", function(){
+navBar.addEventListener("click", function () {
 
     let ulElement = document.querySelector("ul");
     ulElement.classList.toggle("navMenu");
-    
+
 });
 
 //Form submission 
 
 const formElement = document.querySelector("form");
 
-console.log(formElement);
 
 
 
-formElement.addEventListener("submit", function(event){
-    console.log("form is submitted");
+formElement.addEventListener("submit", function (event) {
     event.preventDefault();
     const inputElement = document.getElementById("message");
 
     userMessage = inputElement.value;
-    console.log(userMessage);
 
-    const clearField = function(target){
+    const clearField = function (target) {
         target.value = "";
     }
 
@@ -36,6 +33,5 @@ formElement.addEventListener("submit", function(event){
         clearField(userName);
         clearField(userEmail);
     }
-    
-});
 
+});
